99db0794f15ff39872f2b202df952ee321477c99
Marco Kang <kmnkit@gmail.com>
2021-06-07 00:07:18 +0900

    app url modify

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # django-instagram-backend(with DRF)
 
- Django Rest Framework로 Instagram의 Backend를 클론코딩 해 보았습니다.
+## Django Rest Framework로 Instagram의 Backend를 클론코딩 해 보았습니다.
 
-### API 명세서는 아래의 URL을 참조 바랍니다.
+API 명세서는 아래의 URL을 참조 바랍니다.
 https://documenter.getpostman.com/view/12230851/TzY4fumj
--- app/app/urls.py
+++ app/app/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.conf import settings
 from django.conf.urls import include
 from django.contrib import admin
@@ -21,8 +20,13 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
     path("users/", include("users.urls", namespace="users")),
     path("posts/", include("posts.urls", namespace="posts")),
     path("follows/", include("follows.urls", namespace="follows")),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
