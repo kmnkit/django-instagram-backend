0f46c7bf24fc25362a6e8e93ff3a154e4f28ba22
Marco Kang <kmnkit@gmail.com>
2021-06-17 02:07:46 +0900

    logging edit

--- app/app/settings.py
+++ app/app/settings.py
@@ -215,7 +215,7 @@ LOGGING = {
             "level": "INFO",
             "filters": ["require_debug_false"],
             "class": "logging.handlers.RotatingFileHandler",
-            "filename": BASE_DIR / "logs/app.log",
+            "filename": "logs/app.log",
             "maxBytes": 1024 * 1024 * 5,  # 5 MB
             "backupCount": 5,
             "formatter": "standard",
