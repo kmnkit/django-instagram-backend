be1e3630f6a9cc110eb120b8e6eb619013fb3a37
Marco Kang <kmnkit@gmail.com>
2021-06-17 01:20:16 +0900

    debug edit

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+print(DEBUG)
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
